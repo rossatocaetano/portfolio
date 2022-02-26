b19b3d01c432e944904a3bbaa2526eff64e689b3
Raphael Rossato Caetano <rossato.caetano@gmail.com>
2022-02-25 23:25:12 -0300

    add header

---
+++ src/Components/Navbar/Menu/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {IMenu} from '../../../types/menu';
+import style from './Menu.module.scss';
+
+const Menu = (props: IMenu) => {
+    return (
+        <ul className={style.menu}>
+            {props.links.map((link, index) => {
+                return (
+                    <li className={style.menu__list} key={index}>
+                        {link}
+                    </li>)
+            })}
+        </ul>
+        
+    )
+}
+
+export default Menu;
---
+++ src/Components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Menu from './Menu/index'
+import {IMenu} from '../../types/menu';
+import style from './Navbar.module.scss'
+import logo from '../../assets/logo.png'
+
+const Navbar = (props: IMenu) => {
+    return (
+        <header className={style.navbar}>
+            <figure className={style.logoBox} >
+                <img height={100} className={style.logo} alt="logo" src={logo} />
+            </figure>
+        <nav>
+            <Menu links={props.links}></Menu>         
+        </nav>
+        </header>
+        
+    )
+}
+
+
+
+export default Navbar;
